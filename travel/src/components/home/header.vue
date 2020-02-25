e6759f7f422a1cff234eb674f9c9efa9f918dcdc
mamba666 <lwjkkbb@sina.com>
2020-02-25 20:43:49 +0800

    header开发

--- travel/src/components/home/header.vue
+++ travel/src/components/home/header.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>1</div>
+    <div class="header">
+        <div class="left">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#iconfanhui1"></use>
+            </svg>
+        </div>
+        <div class="input">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#iconsousuo1"></use>
+            </svg>
+            输入城市/景点/主题
+        </div>
+        <div class="right">
+            城市
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#iconiconzhengli-"></use>
+            </svg>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +27,32 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+    @import "~styles/varibles.stylus"
+    .header
+        display flex
+        height .86rem
+        background $bgColor
+        line-height .86rem
+        color #fff
+        text-align center
+        .left
+            width .64rem
+            .icon
+                font-size .3rem
+        .input
+            flex 1
+            background #fff
+            border-radius .1rem
+            box-sizing border-box
+            margin .1rem
+            height .66rem
+            line-height .66rem
+            text-align center
+            color #ccc
+            .icon
+                font-size .3rem
+        .right
+            width 1.24rem
+            .icon
+                font-size .4rem
 </style>
--- travel/src/main.js
+++ travel/src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//引入css  @ 代表 src/
+import "styles/reset.css"
+import "styles/border.css"
+import "styles/iconfont.css"
+import "styles/iconfont.css"
+import "styles/icon.css"
+
+
+import "styles/iconfont.js"
+
 Vue.config.productionTip = false
 
 new Vue({
