e360c40829a0a91ef2fd19867e00f8db8c8521ac
mamba666 <lwjkkbb@sina.com>
2020-02-26 20:43:19 +0800

    fuckgit add -A

--- travel/src/router/home/Home.vue
+++ travel/src/router/home/Home.vue
@@ -4,6 +4,7 @@
         <home-swiper></home-swiper>
         <home-icons></home-icons>
         <home-recommend></home-recommend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
@@ -12,13 +13,15 @@ import HomeHeader from "@/components/home/header"
 import HomeSwiper from "@/components/home/swiper"
 import HomeIcons from "@/components/home/icons"
 import HomeRecommend from "@/components/home/recommend"
+import HomeWeekend from "@/components/home/weekend"
 export default {
     name:"Home",
     components:{
         HomeHeader,
         HomeSwiper,
         HomeIcons,
-        HomeRecommend
+        HomeRecommend,
+        HomeWeekend
     }
 }
 </script>
