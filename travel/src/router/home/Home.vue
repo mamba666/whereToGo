6e5f343f30dfea6a5b793d30c122331c218c5a1f
mamba666 <lwjkkbb@sina.com>
2020-02-26 20:34:03 +0800

    weekend

--- travel/src/router/home/Home.vue
+++ travel/src/router/home/Home.vue
@@ -3,6 +3,8 @@
         <home-header></home-header>
         <home-swiper></home-swiper>
         <home-icons></home-icons>
+        <home-recommend></home-recommend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
@@ -10,12 +12,16 @@
 import HomeHeader from "@/components/home/header"
 import HomeSwiper from "@/components/home/swiper"
 import HomeIcons from "@/components/home/icons"
+import HomeRecommend from "@/components/home/recommend"
+import HomeWeekend from "@/components/home/weekend"
 export default {
     name:"Home",
     components:{
         HomeHeader,
         HomeSwiper,
-        HomeIcons
+        HomeIcons,
+        HomeRecommend,
+        HomeWeekend
     }
 }
 </script>
