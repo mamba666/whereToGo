7944fe603b5b18c342145203d82d7f40c27999a4
mamba666 <lwjkkbb@sina.com>
2020-02-27 14:59:40 +0800

    search

---
+++ travel/src/components/city/search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="search" placeholder="请输入城市或者拼音">
+    </div>
+</template>
+
+<script>
+export default {
+    name:"CitySearch"
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import "~styles/varibles.stylus"
+    .search
+        height .72rem
+        padding 0 .1rem
+        background $bgColor
+        .search-input
+            box-sizing border-box
+            padding 0 .14rem
+            line-height .62rem
+            height .62rem
+            text-align center
+            width 100%
+            border-radius .5rem
+            color pink
+</style>
--- travel/src/router/city/City.vue
+++ travel/src/router/city/City.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import CityHeader from "@/components/city/header"
+import CitySearch from "@/components/city/search"
 
 export default {
     name:"City",
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
