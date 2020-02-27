4ed1f8b349657c38d9424a3290dd9256c0e94ac9
mamba666 <lwjkkbb@sina.com>
2020-02-27 09:02:09 +0800

    axios跨域以及获取mooc

--- travel/src/router/home/Home.vue
+++ travel/src/router/home/Home.vue
@@ -14,6 +14,8 @@ import HomeSwiper from "@/components/home/swiper"
 import HomeIcons from "@/components/home/icons"
 import HomeRecommend from "@/components/home/recommend"
 import HomeWeekend from "@/components/home/weekend"
+import axios from "axios"
+
 export default {
     name:"Home",
     components:{
@@ -22,6 +24,20 @@ export default {
         HomeIcons,
         HomeRecommend,
         HomeWeekend
+    },
+    methods:{
+        getHomeInfo(){
+            //首先 访问8080， axios去请求这么一个路径  
+            //接下来，这个路径会到 vue.config.js中
+            axios.get("/api/index.json").then(this.getHomeInfoSucc)
+        },
+        getHomeInfoSucc(res){
+            //这里可以在控制台中打印
+            console.log(res)
+        }
+    },
+    mounted(){
+        this.getHomeInfo()
     }
 }
 </script>
--- travel/vue.config.js
+++ travel/vue.config.js
@@ -1,13 +1,43 @@
 const path = require('path');
+const mockdata=require("./mock/index.json")
 
 function resolve(dir) {
     return path.join(__dirname, dir);
-    }
-    module.exports = {
+}
+
+
+module.exports = {
     lintOnSave: true,
     chainWebpack: (config) => {
         config.resolve.alias
         .set('styles',resolve('src/assets/styles'))
         // 这里只写了两个个，你可以自己再加，按这种格式.set('', resolve(''))
+    },
+    devServer:{
+        //当Home.vue中的路径过来时，/api/index.json   因为是api开头，
+        //所以这个路径被跨域到 http://localhost:8080/mock/index.json 
+        //即实际上访问8080时，访问的是http://localhost:8080/mock/index.json
+        //这样就拿到了mooc的数据。
+        //拿到之后，就回到vue开始执行then。   
+        //到此结束！！！！
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                pathRewrite:{
+                    '^/api':'/mock'
+                }
+            },
+            '/foo': {
+            target: 'http://localhost:8080'
+            }
+        },
+        //用来显示假数据，即如果直接访问 http://localhost:8080/api/index.json
+        //就会在页面打印mock数据
+        port:8080,
+        before(app){
+            app.get('/api/index.json',(req,res,next)=>{
+                res.json(mockdata)
+            })
+        }
     }
 };
